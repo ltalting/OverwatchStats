b17f8d747d5f1ab184faadd7c56703c9902b5d4d
Landon Alting <lalting@purdue.edu>
2020-11-11 18:18:40 -0500

    Added project

---
+++ OverwatchStats/OverwatchStats/OverwatchStats/mainForm.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace OverwatchStats
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPlatform = new System.Windows.Forms.Label();
+            this.lblRegion = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbPlatform = new System.Windows.Forms.ComboBox();
+            this.cmbRegion = new System.Windows.Forms.ComboBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblPlatform
+            // 
+            this.lblPlatform.AutoSize = true;
+            this.lblPlatform.Location = new System.Drawing.Point(12, 9);
+            this.lblPlatform.Name = "lblPlatform";
+            this.lblPlatform.Size = new System.Drawing.Size(72, 20);
+            this.lblPlatform.TabIndex = 0;
+            this.lblPlatform.Text = "Platform:";
+            // 
+            // lblRegion
+            // 
+            this.lblRegion.AutoSize = true;
+            this.lblRegion.Location = new System.Drawing.Point(12, 54);
+            this.lblRegion.Name = "lblRegion";
+            this.lblRegion.Size = new System.Drawing.Size(64, 20);
+            this.lblRegion.TabIndex = 1;
+            this.lblRegion.Text = "Region:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ID/BattleTag:";
+            // 
+            // cmbPlatform
+            // 
+            this.cmbPlatform.FormattingEnabled = true;
+            this.cmbPlatform.Items.AddRange(new object[] {
+            "PlayStation Network",
+            "Xbox LIVE",
+            "PC"});
+            this.cmbPlatform.Location = new System.Drawing.Point(121, 6);
+            this.cmbPlatform.Name = "cmbPlatform";
+            this.cmbPlatform.Size = new System.Drawing.Size(211, 28);
+            this.cmbPlatform.TabIndex = 3;
+            // 
+            // cmbRegion
+            // 
+            this.cmbRegion.FormattingEnabled = true;
+            this.cmbRegion.Items.AddRange(new object[] {
+            "United States",
+            "Europe",
+            "Asia"});
+            this.cmbRegion.Location = new System.Drawing.Point(121, 51);
+            this.cmbRegion.Name = "cmbRegion";
+            this.cmbRegion.Size = new System.Drawing.Size(211, 28);
+            this.cmbRegion.TabIndex = 4;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(121, 96);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(211, 26);
+            this.txtID.TabIndex = 5;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(121, 142);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(121, 35);
+            this.btnGenerate.TabIndex = 6;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 189);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.cmbRegion);
+            this.Controls.Add(this.cmbPlatform);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblRegion);
+            this.Controls.Add(this.lblPlatform);
+            this.Name = "mainForm";
+            this.Text = "Overwatch Stats";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPlatform;
+        private System.Windows.Forms.Label lblRegion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbPlatform;
+        private System.Windows.Forms.ComboBox cmbRegion;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Button btnGenerate;
+    }
+}
+
---
+++ OverwatchStats/OverwatchStats/OverwatchStats/mainForm.cs
@@ -0,0 +1,107 @@
+﻿using Aspose.Cells;
+using Aspose.Cells.Utility;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OverwatchStats
+{
+    public partial class mainForm : Form
+    {
+        public string platform;
+        public string region;
+        public string id;
+
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            int selectedPlatform = cmbPlatform.SelectedIndex;
+
+            switch (selectedPlatform)
+            {
+                case 0:
+                    platform = "psn";
+                    break;
+                case 1:
+                    platform = "xbl";
+                    break;
+                case 2:
+                    platform = "pc";
+                    break;
+            }
+
+            int selectedRegion = cmbRegion.SelectedIndex;
+
+            switch (selectedRegion)
+            {
+                case 0:
+                    region = "us";
+                    break;
+                case 1:
+                    region = "eu";
+                    break;
+                case 2:
+                    region = "asia";
+                    break;
+            }
+
+            id = txtID.Text;
+            string url = "https://ow-api.com/v1/stats/" + platform + "/" + region + "/" + id + "/profile";
+            Request(url);
+        }
+
+        async static void Request(string url)
+        {
+            string path = @"C:\temp\results.json";
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.GetAsync(url))
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        using (FileStream fs = File.Create(path))
+                        {
+                            string statContent = "";
+                            byte[] info = new UTF8Encoding(true).GetBytes(statContent = await content.ReadAsStringAsync());
+                            // Add some information to the file.
+                            fs.Write(info, 0, info.Length);
+                        }
+                        MakeExcelFile();
+                    }
+                }
+            }
+        }
+
+        private static void MakeExcelFile()
+        {
+            Workbook workbook = new Workbook();
+            Worksheet worksheet = workbook.Worksheets[0];
+            string resultPath = @"C:\temp\results.json";
+
+            // Read JSON File
+            string jsonInput = File.ReadAllText(resultPath);
+
+            // Set JsonLayoutOptions
+            JsonLayoutOptions options = new JsonLayoutOptions();
+            options.ArrayAsTable = true;
+
+            // Import JSON Data
+            JsonUtility.ImportData(jsonInput, worksheet.Cells, 0, 0, options);
+
+            // Save Excel file
+            workbook.Save("stats.xlsx");
+        }
+    }
+}
